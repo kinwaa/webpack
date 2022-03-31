e213de6a91392c4d02ed719e2c359c547fe5f8eb
梁建华 <andy_leung@163.com>
2022-03-31 16:46:53 +0800

    完成《Output Management》

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path'); // 先不要Quick Fix
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
    mode: 'development',
-   entry: './src/index.js',
-   output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
+   entry: {
+      index: './src/index.js',
+      print: './src/print.js'
    },
+   plugins: [
+      new HtmlWebpackPlugin({
+         title: 'Output Management'
+      })
+   ],
+   output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+   }
 };
