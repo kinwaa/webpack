feacde46116c958e80c5ba8d02a50617d6c28c26
梁建华 <andy_leung@163.com>
2022-03-31 15:15:03 +0800

    完成《Asset Management》中的Loading Data

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,12 @@ module.exports = {
       }, {
          test: /\.(woff|woff2|eot|ttf|otf)$/i,
          type: 'asset/resource'
+      }, {
+         test: /\.(csv|tsv)$/i,
+         use: ['csv-loader']
+      }, {
+         test: /\.xml$/i,
+         use: ['xml-loader']
       }]
    }
 };
