1f25f2e1091431b39aacf4a6fe666f5338c91863
梁建华 <andy_leung@163.com>
2022-03-31 14:49:42 +0800

    完成《Asset Management》中的Loading Images

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
       rules: [{
          test: /\.css$/i,
          use: ['style-loader', 'css-loader']
+      }, {
+         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+         type: 'asset/resource'
       }]
    }
 };
