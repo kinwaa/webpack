b744fb5846b304689d4bade273894688dada2b8d
梁建华 <andy_leung@163.com>
2022-03-31 18:21:07 +0800

    完成《Code Splitting》的SplitChunksPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -18,5 +18,10 @@ module.exports = {
    output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist')
+   },
+   optimization: {
+      splitChunks: {
+         chunks: 'all'
+      }
    }
 };
