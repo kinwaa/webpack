cd25b185f06053682a7b07cc14db1d78aa2f3bdc
梁建华 <andy_leung@163.com>
2022-03-31 17:37:08 +0800

    完成《Code Splitting》的Entry Points

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require('path'); // 先不要Quick Fix
 
 module.exports = {
    mode: 'development',
-   entry: './src/index.js',
-   output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
+   entry: {
+      index: './src/index.js',
+      another: './src/another-module.js'
    },
+   output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+   }
 };
