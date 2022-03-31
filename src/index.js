d13c9c61ce64556833c5f138259036c7bbe36770
Kinwaa <andy19feb@gmail.com>
2022-03-31 20:57:08 +0800

    试试async function的写法

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 
-function getComponent() {
-   return import('lodash').then(({ default: _ }) => {
+async function getComponent() {
+   try {
+      const { default: _ } = await import('lodash');
       const element = document.createElement('div');
 
       // Lodash, now imported by this script
       element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   
+
       return element;
-   }).catch(() => {
+   } catch {
       return "An error occurred while loading the component";
-   });
+   }
 }
 
 getComponent().then((component) => {
